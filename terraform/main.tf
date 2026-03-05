552133551a0c64e74ed3e7539a1340c41a119773
naiyar-imam <naiyar.imam@bellblazetech.com>
2026-03-05 16:17:21 +0530

    final test

--- terraform/main.tf
+++ terraform/main.tf
@@ -75,8 +75,15 @@ resource "aws_instance" "app_server" {
   user_data = <<-EOF
 #!/bin/bash
 
-apt update -y
-apt install -y docker.io awscli
+sudo apt update
+sudo apt install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin - y
 
 systemctl start docker
 systemctl enable docker
