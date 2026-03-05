cb526610cda47dc6c7202a5fdbe4c8014c33b2fa
naiyar-imam <naiyar.imam@bellblazetech.com>
2026-03-05 10:49:12 +0530

    server changes to ubuntu

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,15 +2,17 @@ provider "aws" {
   region = var.region
 }
 
+# Security Group
 resource "aws_security_group" "app_sg" {
   name_prefix = "devops-sg-"
-  description = "Allow SSH, HTTP, HTTPS"
+  description = "Allow SSH, HTTP, HTTPS, Backend"
 
   lifecycle {
     create_before_destroy = true
   }
 
   ingress {
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -18,6 +20,7 @@ resource "aws_security_group" "app_sg" {
   }
 
   ingress {
+    description = "HTTPS"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
@@ -25,8 +28,17 @@ resource "aws_security_group" "app_sg" {
   }
 
   ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Backend App"
+    from_port   = 8000
+    to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -39,22 +51,28 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
+# EC2 Instance
 resource "aws_instance" "app_server" {
-  ami           = "ami-0f58b397bc5c1f2e8" # Amazon Linux
+  ami           = "ami-0f58b397bc5c1f2e8"  # Ubuntu AMI
   instance_type = "t2.micro"
-
-  # IMPORTANT: Use ID, not name
-  vpc_security_group_ids = [aws_security_group.app_sg.id]
 
   key_name = "devops-key"
 
+  vpc_security_group_ids = [
+    aws_security_group.app_sg.id
+  ]
+
+  associate_public_ip_address = true
+
   user_data = <<-EOF
 #!/bin/bash
-yum update -y
-yum install docker -y
+apt update -y
+apt install docker.io -y
+
 systemctl start docker
 systemctl enable docker
-usermod -aG docker ec2-user
+
+usermod -aG docker ubuntu
 EOF
 
   tags = {
